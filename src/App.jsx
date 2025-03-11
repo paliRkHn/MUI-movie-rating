8487a409dc0fea396ac3107ab670d25f822a6968
pali-rojas <paloma.rkhn@gmail.com>
2025-03-11 22:41:21 +1100

    components in files, dark theme in navbar applied globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,23 @@
-import React, { useState } from 'react';
-import Grid from '@mui/material/Grid2';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { createTheme, Typography, FormControlLabel, Switch } from '@mui/material';
-import ButtonAppBar from './components/Navbar';
-import MovieCard from './components/Card';
-import actionMovies from './components/movies-list/ActionMovies';
-import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { ThemeContextProvider } from './components/Themes';
+import ActionPage from './components/ActionPage';
+import AnimePage from './components/AnimePage';
+import SciFiPage from './components/SciFiPage';
 
 function App(){
-  
-  const darkTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#2d2062',
-        light: '#656798'
-      },
-      secondary: {
-        main: '#9668b4'
-      },
-      background: '#171033',
-      text: {
-        primary: '#ffffff',
-        secondary: '#000000',
-      }
-    },
-    typography: {
-      fontFamily: 'Trebuchet MS, sans-serif'
-    }
-  })
 
-  const lightTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#2d2062',
-        light: '#9395b7'
-      },
-      secondary: {
-        main: '#a27db6'
-      },
-      background: '#ffffff',
-      text: {
-        primary: '#000000',
-        secondary: '#ffffff',
-      }
-    },
-    typography: {
-      fontFamily: 'Trebuchet MS, sans-serif'
-    }
-  })
-
-  const [isDark, setTheme] = useState('light');
-
-  const selectTheme = () => {
-    setTheme(prevMode => prevMode === 'light' ? 'dark': 'light')
-  };
-
-  const theme = isDark === 'light' ? lightTheme : darkTheme;
   
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ top: 0, left: 0, pb: 3, backgroundColor: 'background'}}>
-        <Grid container spacing={3} sx={{ top: 0, left: 0, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Grid size={12}>
-            <ButtonAppBar />
-          </Grid>
-          <Grid size={9}>
-            <Typography variant='h3' sx={{ pl: '3rem', pt: 1, color: 'text.primary' }}>
-              Movies about...
-            </Typography>
-          </Grid>
-          <Grid container size={3}  sx={{ justifyContent: 'right', alignSelf: 'flex-start'}}>
-            <FormControlLabel label="Dark Mode"  sx={{ color: 'text.primary'}} control={
-              <Switch color='secondary' checked={isDark === "dark"} onChange={selectTheme}/>
-            } 
-            />
-          </Grid>
-          <Grid size={8}>
-            <Paper elevation={3} sx={{ backgroundColor: 'secondary.main', borderRadius: 3, mb: '1rem'}}>
-              <Typography variant='h2' sx={{ display: 'flex', justifyContent:'center', py: 2}}>
-                Badass people and guns
-              </Typography>
-            </Paper>
-          </Grid>
-          
-          <Grid container justifyContent={'center'} sx={{ px: 2}}>
-            {actionMovies.map((movie, i) => (
-              <Grid item key={i} size={{ md:3, sm:4, xs:8  }}>
-                <MovieCard {...movie} />
-              </Grid>
-            ))} 
-          </Grid>
-          <Grid size={{ md:5, xs:8 }}>
-            <Paper elevation={10} sx={{ mx: 2, my: 1, px: 8, py: 4, backgroundColor: 'primary.main'}}>
-              <Typography variant='h6' sx={{textAlign: 'center', color: 'white'}}>
-                Give your rate to some of my favorite movies. 
-                <Typography variant='body1' sx={{ pt: "1rem", pb: '2rem'}}>(These are all 5 stars for me anyways 🤩)</Typography>
-                xoxo
-                <br />
-                Paloma
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Box>
-    </ThemeProvider>
+
+    //add Routes
+
+    <ThemeContextProvider>
+      <Navbar />
+      <ActionPage />
+      <AnimePage />
+      <SciFiPage />
+    </ThemeContextProvider>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+import { useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,8 +7,17 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { useTheme } from '@mui/material/styles';
+import { ThemeModeContext } from './Themes';
 
-export default function ButtonAppBar() {
+
+export default function Navbar() {
+  
+  const theme = useTheme();
+  const colorMode = useContext(ThemeModeContext);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: 'primary.main' }} >
@@ -25,6 +35,10 @@ export default function ButtonAppBar() {
             Movies Rating
           </Typography>
           <Button variant="contained" sx={{ backgroundColor: 'primary.light', color: 'text.primary' }}>Login</Button>
+          <FormControlLabel label="Dark Mode"  sx={{ color: 'text.primary'}} control={
+                        <Switch color='secondary' checked={theme.palette.mode === 'dark'} onChange={colorMode.switchColorMode}/>
+                      } 
+                      />
         </Toolbar>
       </AppBar>
     </Box>
